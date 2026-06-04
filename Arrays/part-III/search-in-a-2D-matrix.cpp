3294055640427fd7962f61dfa1298ead319dabf8
sajidansari7 <ansarisajid2063@gmail.com>
2026-06-04 20:28:28 +0530

    Create search-in-a-2D-matrix.cpp

---
+++ Arrays/part-III/search-in-a-2D-matrix.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        // flatten the 2D matrix into 1D matrix
+        int n=matrix.size(),m=matrix[0].size();
+        int low=0,high=m*n-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            int row=mid/m;
+            int col=mid%m;
+            if(matrix[row][col]==target) return true;
+            else if(matrix[row][col]<target) low=mid+1;
+            else high=mid-1;
+        }
+        return false;
+        
+    }
+};
